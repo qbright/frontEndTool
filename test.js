46d40d458e52998d6a63b1f3b6465d345c15ccc7
zqbright <zqbright@gmail.com>
2013-09-22 17:36:59 +0800

    ~
    Signed-off-by: zqbright <zqbright@gmail.com>

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-var imagemin = require("imagemin");
-
-
-imagemin("test/","test/dist",function(){
-	console.log("finish");
+var pngquantPath = require("pngquant-bin").path,
+    cluster = require("cluster"),
+    numCpus = require("os").cpus().length,
+    execFile = require('child_process').execFile,
+    fs = require("fs"),
+    path = require("path");
+console.time("a");
+var pathArray = [];
+fs.readdir("test/",function(err,files){
+    for(var i = 0,file;file = files[i];i++){
+        var filePath = path.join("test/",file);
+        if(fs.statSync(filePath).isFile()){
+            pathArray.push(filePath);
+        }
+    }
+    execFile(pngquantPath,pathArray, function() {
+        console.log('Image minified');
+        console.timeEnd("a");
+    });
 });
+
+
+
